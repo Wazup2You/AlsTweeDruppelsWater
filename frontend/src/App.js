9fcd78139a7f35b6c123c5fb1283004bc819a21d
LisaKeers <57112640+LisaKrs@users.noreply.github.com>
2022-03-22 17:08:19 +0100

    fix

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -2,18 +2,10 @@ import './App.css';
 
 function App() {
   return (
-<<<<<<< HEAD
-    <div className=" bg-red-600">
-      <header className="">
-        src/App.js is de index pagina, hierin moeten de componenten uit het
-        componenten mapje geladen worden hi
-      </header>
-=======
     <div className=" w-screen h-screen bg-blue-oasen">
       <h1 className=''>
        In deze file komen de components uit het components mapje
       </h1>
->>>>>>> 78aea6d8d5e9ef139fd7526abb8fa28689b295a7
     </div>
   );
 }
