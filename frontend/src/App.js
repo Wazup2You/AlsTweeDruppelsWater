d3377a029059aef44d8fd8616900c29b5b631090
eva <evavanderkroft@gmail.com>
2022-03-28 15:33:16 +0200

    updates on routing/scenes

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -3,18 +3,40 @@ import "./App.css";
 import { Button } from "./components/Button/Component";
 
 function App() {
-  const [text, setText] = useState("hoi")
+  const [scene, setScene] = useState(0);
 
   return (
-    <div className=" w-screen h-screen bg-blue-oasen">
-      <h1 className="">
-        In deze file komen de components uit het components mapje
-      </h1>
-      
-      <Button text={text} onClick={() => setText("test")} />
-
+    <div>
+      <div className=" w-screen h-screen bg-white flex flex-col items-center justify-center ">
+        <h1 className="w-200 h-20 text-center">{scene === 0 ? "Intrigerende en Pakkende titel" : scene === 1 ? "Test je kennis" : scene === 2 ? "Tips" : scene === 3 ? "Visuals" : ""}
+        </h1>
+        <h3 className="w-200 h-20 text-center">{scene === 0 ? "Intrigerende en Pakkende ondertitel" : scene === 1 ? "Intrigerende en Pakkende ondertitel 2" : scene === 2 ? "Intrigerende en Pakkende ondertitel 3" : scene === 3 ? "Intrigerende en Pakkende ondertitel 4" : ""}
+        </h3>
+        <div classname="">
+          <div className={scene === 1 ? "hidden" : "block"} >
+            <Button text="Test je kennis" onClick={() => setScene(1)} />
+          </div>
+          <div className={scene === 2 ? "hidden" : "block"} >
+            <Button text="Tips" onClick={() => setScene(2)} />
+          </div>
+          <div className={scene === 3 ? "hidden" : "block"} >
+            <Button text="Visuals" onClick={() => setScene(3)} />
+          </div>
+        </div>
+        <div className="flex justify-between">
+          <div className={scene === 1 ? "block" : "hidden"} >
+            <Button text="oasen Drinkwater" onClick={() => ""} />
+          </div>
+          <div className={scene === 1 ? "block" : "hidden"} >
+            <Button text="Store Drinkwater" onClick={() => ""} />
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
+
+
+
 
 export default App;
--- frontend/src/components/Button/Component.js
+++ frontend/src/components/Button/Component.js
@@ -1,12 +1,15 @@
 import React from "react";
 
 export function Button(props) {
+  const classes = "bg-blue-oasen w-60 h-20 m-4 hover:bg-blue-500 text-white font-bold rounded"
   if (props.href !== undefined) {
     return (
-      <button type="button" class="btn btn-blue">
+
+      <button type="button" className={classes}>
         <a href={props.href}>{props.text}</a>
       </button>
+
     );
-  } else return <button onClick={props.onClick}>{props.text}</button>;
+  } else return <button className={classes} onClick={props.onClick}>{props.text}</button>;
 }
 
