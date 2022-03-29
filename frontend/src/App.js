1f5b89139d7d844a04efd49c5eb7734974d30501
LisaKeers <57112640+LisaKrs@users.noreply.github.com>
2022-03-29 14:28:07 +0200

    comments en tips begin

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -6,27 +6,15 @@ function App() {
   const [scene, setScene] = useState(0);
 
   return (
-<<<<<<< HEAD
-    <div className="w-screen h-screen bg-blue-oasen">
-      <h1 className="text-center">
-        {scene === 0 ?  "scene 1" : scene === 1 ? "scene 2" : scene === 2 ? "scene 3" : ""}
-      </h1>
-
-      <div className="flex flex-col">
-        <Button text="scene 1" onClick={() => setScene(0)} />
-
-        <Button text="scene 2" onClick={() => setScene(1)} />
-
-        <Button text="scene 3" onClick={() => setScene(2)} />
-      </div>
-
-=======
     <div>
-      <div className=" w-screen h-screen bg-white flex flex-col items-center justify-center ">
+      {/*titels*/}
+      <div className=" w-screen h-screen bg-white flex flex-col items-center justify-center">
         <h1 className="w-200 h-20 text-center">{scene === 0 ? "Intrigerende en Pakkende titel" : scene === 1 ? "Test je kennis" : scene === 2 ? "Tips" : scene === 3 ? "Visuals" : ""}
         </h1>
         <h3 className="w-200 h-20 text-center">{scene === 0 ? "Intrigerende en Pakkende ondertitel" : scene === 1 ? "Intrigerende en Pakkende ondertitel 2" : scene === 2 ? "Intrigerende en Pakkende ondertitel 3" : scene === 3 ? "Intrigerende en Pakkende ondertitel 4" : ""}
         </h3>
+
+        {/*navigatie*/}
         <div classname="">
           <div className={scene === 1 ? "hidden" : "block"} >
             <Button text="Test je kennis" onClick={() => setScene(1)} />
@@ -38,7 +26,9 @@ function App() {
             <Button text="Visuals" onClick={() => setScene(3)} />
           </div>
         </div>
-        <div className="flex justify-between">
+
+        {/*quiz*/}
+        <div className="w-10/12 flex justify-between">
           <div className={scene === 1 ? "block" : "hidden"} >
             <Button text="oasen Drinkwater" onClick={() => ""} />
           </div>
@@ -46,13 +36,24 @@ function App() {
             <Button text="Store Drinkwater" onClick={() => ""} />
           </div>
         </div>
+
+        {/*tips*/}
+        <div className={scene === 2 ? "block" : "hidden"} >
+            <Button text="Nieuwe tip" onClick={() => ""} />
+        </div>
+
+        <div className="w-10/12 flex justify-between">
+          <div className={scene === 2 ? "inline" : "hidden"} >
+            <p>Een hele interesante tip</p>
+          </div>
+          <div className={scene === 2 ? "inline" : "hidden"} >
+            <p>Een hele interesante tip</p>
+          </div>
+        </div>
+
       </div>
->>>>>>> d3377a029059aef44d8fd8616900c29b5b631090
     </div>
   );
 }
-
-
-
 
 export default App;
--- frontend/src/components/Button/Component.js
+++ frontend/src/components/Button/Component.js
@@ -4,15 +4,9 @@ export function Button(props) {
   const classes = "bg-blue-oasen w-60 h-20 m-4 hover:bg-blue-500 text-white font-bold rounded"
   if (props.href !== undefined) {
     return (
-<<<<<<< HEAD
-      <button type="button" className="">
-=======
-
       <button type="button" className={classes}>
->>>>>>> d3377a029059aef44d8fd8616900c29b5b631090
         <a href={props.href}>{props.text}</a>
       </button>
-
     );
   } else return <button className={classes} onClick={props.onClick}>{props.text}</button>;
 }
