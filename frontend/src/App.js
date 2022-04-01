ae244d9224b93d9b337155f126a5d499aaf3cfc7
LisaKeers <57112640+LisaKrs@users.noreply.github.com>
2022-04-01 12:20:22 +0200

    positie fix

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -8,32 +8,29 @@ function App() {
   return (
     <div>
       {/*titels*/}
-      <div className=" w-screen h-screen bg-white flex flex-col items-center justify-center">
-        <h1 className="w-200 h-20 text-center">{"Als twee druppels water"}
-        </h1>
-        <h3 className="w-200 h-20 text-center">{scene === 0 ? "Hoe bewust ben jij?" : scene === 1 || scene === 4 || scene === 5 ? "Welk water is goedkoper?" : scene === 2 ? "Tips om beter gebruik te maken van water" : scene === 3 ? "Klik en sleep de druppels voor meer informatie" : ""}
-        </h3>
+      <div className="w-screen h-screen bg-white flex flex-col items-center justify-center">
+        <div className="absolute mb-60">
+          <h1 className="w-200 h-20 text-center">{"Als twee druppels water"}
+          </h1>
+          <h3 className="w-200 h-20 text-center">{scene === 0 ? "Hoe bewust ben jij?" : scene === 1 || scene === 4 || scene === 5 ? "Welk water is goedkoper?" : scene === 2 ? "Tips om beter gebruik te maken van water" : scene === 3 ? "Klik en sleep de druppels voor meer informatie" : ""}
+          </h3>
 
-        {/*navigatie*/}
-        
-        <div className="">
-          <div className={scene === 0 ? "hidden" : "block"} >
-            <Button text="Visuals" onClick={() => setScene(0)} />
+          {/*navigatie*/}
+          
+          <div className="contents">
+            <div className={scene === 0 ? "hidden" : "flex justify-center"} >
+              <Button text="Visuals" onClick={() => setScene(0)} />
+            </div>
+            <div className={scene === 1 || scene === 4 || scene === 5 ? "hidden" : "flex justify-center"} >
+              <Button text="Test je kennis" onClick={() => setScene(1)} />
+            </div>
+            <div className={scene === 2 ? "hidden" : "flex justify-center"} >
+              <Button text="Tips" onClick={() => setScene(2)} />
+            </div>
+            <div className={scene === 3 ? "hidden" : "flex justify-center"} >
+              <Button text="Vergelijk het water" onClick={() => setScene(3)} />
+            </div>
           </div>
-          <div className={scene === 1 || scene === 4 || scene === 5 ? "hidden" : "block"} >
-            <Button text="Test je kennis" onClick={() => setScene(1)} />
-          </div>
-          <div className={scene === 2 ? "hidden" : "block"} >
-            <Button text="Tips" onClick={() => setScene(2)} />
-          </div>
-          <div className={scene === 3 ? "hidden" : "block"} >
-            <Button text="Vergelijk het water" onClick={() => setScene(3)} />
-          </div>
-        </div>
- 
-        <div className= {scene === 4 || scene === 5 ? " p-10 block w-10/12 flex justify-between"  : "hidden"}>
-            <h1 className="w-200 h-20 text-center flex md:text-green-500" > €0,00117</h1> 
-            <h1 className="w-200 h-20 text-left flex md:text-red-500">€0,70</h1>
         </div>
         
         {/*quiz*/}
@@ -41,14 +38,19 @@ function App() {
         {/* <div>
             <img src={require('./img/417864.png')}></img>
             <img src={require('./img/417864.png')}></img>
-          </div> */}
+        </div> */}
 
-       
-
+        <div className="h-96"></div>
+        <div className="absolute w-10/12">
+          <div className= {scene === 4 || scene === 5 ? "p-10 block flex justify-between"  : "hidden"}>
+              <h1 className="w-200 h-20 text-center flex md:text-green-500" > €0,00117</h1> 
+              <h1 className="w-200 h-20 text-left flex md:text-red-500">€0,70</h1>
+          </div>
+        </div>
 
         <div className="w-10/12 flex justify-between">
           <div className={scene === 1 || scene === 4 || scene === 5 ? "block" : "hidden"} >
-            <Button text="oasen Drinkwater" onClick={() => setScene(4)} scene={scene} />
+            <Button text="Oasen Drinkwater" onClick={() => setScene(4)} scene={scene} />
           </div>   
           <div className={scene === 4 || scene === 5 ? "block w-2/3" : "hidden"}>
           <p className= "p-10 text-center"> Wij zijn van mening dat water betaalbaar moet zijn voor iedereen en maken hier werk van </p>  
@@ -72,7 +74,6 @@ function App() {
             <p>Een hele interesante tip</p>
           </div>
         </div>
-
       </div>
     </div >
   );
