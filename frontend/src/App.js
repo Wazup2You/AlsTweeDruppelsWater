2b79cdca94537ce4b977823728769a053e3fadf4
Inci <inciyesiltepe@gmail.com>
2022-04-04 23:00:29 +0200

    refresh

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -16,6 +16,14 @@ const WaterDropVideo = lazy(() => import("./components/DropVideo/Component"));
 function App() {
   const [scene, setScene] = useState(0);
 
+  const refresh = (scene) => {
+    // re-renders the component
+    setScene(2);
+    setTimeout(() => {
+      setScene(scene);
+    }, 1);
+  };
+
   const scenes = {
     // visuals
     0: {
@@ -174,7 +182,7 @@ function App() {
 
       <div className="absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 pointer-events-auto">
         <div className={scene === 0 ? "hidden" : "block"}>
-          <Button text="Visuals" onClick={() => setScene(0)} />
+          <Button text="Visuals" onClick={() => refresh(0)} />
         </div>
         <div className={scene === 1 ? "hidden" : "block"}>
           <Button text="Vergelijk het water" onClick={() => setScene(1)} />
@@ -183,7 +191,7 @@ function App() {
           <Button text="Test je kennis" onClick={() => setScene(2)} />
         </div>
         <div className={scene === 3 ? "hidden" : "block"}>
-          <Button text="Tips" onClick={() => setScene(3)} />
+          <Button text="Tips" onClick={() => refresh(3)} />
         </div>
       </div>
     </Suspense>
